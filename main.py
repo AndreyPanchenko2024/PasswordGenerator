dc7e716ead0f94cfe7783c54e9c6f33b2c46ba0c
AndreyPanchenko2024 <panchenkoqq@yandex.ru>
2024-02-27 00:10:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import random
+import string
 
+#Набор символов верхнего и нижнего регистров + набор всех цифр + всех знаков препинания
+sequence = string.ascii_letters + string.digits + string.punctuation
+
+#Установление пароля длинной в 20 символов
+lenght = 20
+
+
+password = ''.join(random.sample(sequence, lenght))
+print(password)
+
+ 
